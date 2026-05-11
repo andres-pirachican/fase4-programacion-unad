906212107282fec255cb247cc7d7b63584a27356
andres-pirachican <afpirachicanm@unadvirtual.edu.co>
2026-05-10 19:11:26 -0500

    Se crea clase Cliente (estructura inicial)

---
+++ cliente.py
@@ -0,0 +1,8 @@
+# Clase Cliente
+# Fase 4 - Programación UNAD
+
+class Cliente:
+    def __init__(self, nombre, identificacion, correo):
+        self.nombre = nombre
+        self.identificacion = identificacion
+        self.correo = correo
